e6460eaf2200c4fbd202139efc3547e0c9369e3b
Seoweol <139940350+ahddl622@users.noreply.github.com>
2024-01-16 09:57:16 +0900

    [level Lv.0] Title: 정수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181840. 정수 찾기/정수 찾기.js
@@ -0,0 +1,8 @@
+function solution(num_list, n) {
+    let result = 0
+    for (let i = 0; i <= num_list.length; i++) {
+        if (num_list[i] === n) {
+            result = 1
+        } 
+    } return result
+}
